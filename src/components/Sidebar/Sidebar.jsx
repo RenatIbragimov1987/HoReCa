e1965aad0693690d25fabc214130a3be5361b0af
Renat Ibragimov <naka070787@gmail.com>
2024-02-04 19:34:40 +0300

    commit44

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -19,7 +19,7 @@ function Sidebar({ isContactsPage }) {
           <a
             target="_blank"
             rel="nofollow noreferrer"
-            href="https://telegram.me/td_horeca?start=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%9F%D0%B8%D1%88%D1%83%20%D0%B2%D0%B0%D0%BC%20%D1%81%20%D1%81%D0%B0%D0%B9%D1%82%D0%B0%20td-horeca.ru.%20%20"
+            href="https://t.me/td_horeca?start=Hello%20from%20td-horeca.ru"
             className="sideBar__nav"
           >
             <img src={telegram} alt="logo" className="sideBar__icon" />
