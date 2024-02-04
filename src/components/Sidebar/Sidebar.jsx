c48e1f1cf27baed9b9a5bbaee57c3f088a9ad67d
Renat Ibragimov <naka070787@gmail.com>
2024-02-04 20:03:21 +0300

    commit48

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -10,7 +10,7 @@ function Sidebar({ isContactsPage }) {
         <>
           <a
             target="_blank"
-            href="https://wa.me/+79673902333?text=Здравствуйте!%20Пишу%20вам%20с%20сайта%20td-horeca.ru.</br></br>"
+            href="https://wa.me/+79673902333?text=Здравствуйте!%20Пишу%20вам%20с%20сайта%20td-horeca.ru.  "
             rel="nofollow noreferrer"
             className="sideBar__nav"
           >
